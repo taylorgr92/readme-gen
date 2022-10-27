7683d7061601a88c0425cccfce4f84acc1961d04
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-27 17:58:25 -0500

    add inquirer questions

--- index.js
+++ index.js
@@ -3,30 +3,54 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 //Create array of questions
-inquirer
-.prompt = ([
+// inquirer.prompt = [
 //   {
 //     type: "input",
-//     message: "Choose a license for you application",
-//     licenses: {
-//       license: "",
-//     },
-//     name: "license",
+//     message: "What is your location?",
+//     name: "location",
+//     choice: "",
 //   },
-  {
-    type: "input",
-    message: "What is your location?",
-    name: "location",
-  },
-  {
-    type: "input",
-    message: "Enter your email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "What is your Github URL?",
-    name: "github",
-  },
+//   {
+//     type: "input",
+//     message: "What is your email address?",
+//     name: "email",
+//   },
+//   {
+//     type: "input",
+//     message: "What is your Github URL?",
+//     name: "github",
+//   },
+// ].then((data) => {
+//   const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
 
-]);
+//   fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+//     err ? console.log(err) : console.log("Success!")
+//   );
+// });
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your location?",
+    },
+    {
+
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your Github URL?",
+      name: "github",
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });

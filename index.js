412cb0dd75c6f79b2266cbd0352e74886d8f0c48
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-27 18:04:21 -0500

    html doc skeleton

--- index.js
+++ index.js
@@ -2,31 +2,23 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-//Create array of questions
-// inquirer.prompt = [
-//   {
-//     type: "input",
-//     message: "What is your location?",
-//     name: "location",
-//     choice: "",
-//   },
-//   {
-//     type: "input",
-//     message: "What is your email address?",
-//     name: "email",
-//   },
-//   {
-//     type: "input",
-//     message: "What is your Github URL?",
-//     name: "github",
-//   },
-// ].then((data) => {
-//   const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
-
-//   fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
-//     err ? console.log(err) : console.log("Success!")
-//   );
-// });
+const genHTML = ({ location, email, github, }) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    
+  </div>
+</div>
+</body>
+</html>`;
 
 inquirer
   .prompt([

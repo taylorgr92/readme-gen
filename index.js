1e5498d06e371f709c73ea2493720b3caef3c227
taylorgr92 <79235815+taylorgr92@users.noreply.github.com>
2022-10-27 13:57:34 -0500

    add questions to array

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-//packges required for app
+//Packges required for application
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+//Create array of questions
+inquirer
+.prompt = ([
+//   {
+//     type: "input",
+//     message: "Choose a license for you application",
+//     licenses: {
+//       license: "",
+//     },
+//     name: "license",
+//   },
+  {
+    type: "input",
+    message: "What is your location?",
+    name: "location",
+  },
+  {
+    type: "input",
+    message: "Enter your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your Github URL?",
+    name: "github",
+  },
 
+]);
